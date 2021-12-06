f4031eac3e9871aea9919019ca8a636cdf8aae82
jashgada <jashgada93@gmail.com>
2021-12-05 22:39:20 -0500

    add sorting and file uploads

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import csv
 # import os
 # import json
-from flask import Flask
+from flask import Flask, request
 import pyodbc
 # import pandas as pd
 # from flask_restful import reqparse, Api
@@ -49,42 +49,134 @@ def count_me(input_str):
 def test():
     return "This is test"
 
-@app.route('/query/get')
-def post():
-        """ Retrieves data from the database """
-        sql_statement = "SELECT TOP (1000) * FROM [SalesLT].[Customer]"
 
+
+# @app.route('/createDb')
+# def createDb():
+#     cursor = conn.cursor()  
+#     cursor.execute("""DROP TABLE IF EXISTS household""")
+#     cursor.execute("""CREATE TABLE household (HSHD_NUM nvarchar(MAX), L nvarchar(MAX), AGE_RANGE nvarchar(MAX), MARITAL nvarchar(MAX), INCOME_RANGE nvarchar(MAX), HOMEOWNER nvarchar(MAX), HSHD_COMPOSITION nvarchar(MAX), HH_SIZE nvarchar(MAX), CHILDREN nvarchar(MAX))""")
+
+#     with open('400_households.csv', 'r') as f:
+#         reader = csv.reader(f.readlines()[1:])  # exclude header line
+#         cursor.executemany("""INSERT INTO household VALUES (?,?,?,?,?,?,?,?,?)""",
+#                     (row for row in reader))
+#     cursor.commit()
+#     cursor.close()
+#     return "success!"
+
+@app.route('/display')
+def display():
+    formhyptertext = """
+     <form action="/displayData" method = "POST">
+    <p>Sort By <input type = "text" name = "sortBy" /></p>
+    <p>Yyou can sort by Hshd_num, Basket_num, Date, Product_num, Department, Commodity</p>
+    <p><input type = "submit" value = "Submit" /></p>
+</form>
+    """
+    return formhyptertext
+    # sql_statement = "SELECT TOP (1000) * FROM [household]"
+
+    # cursor = conn.cursor()    
+    # rows = cursor.execute(sql_statement).fetchall()
+    # cursor.close()
+    # return '<br><br>'.join(str(row) for row in rows)
+@app.route('/displayData/', methods = ["POST", "GET"])
+def displayData():
+    if request.method == "POST":
+        sorter =  request.form["sortBy"]
+        sql_statement = """SELECT TOP (1000) * FROM [household] WHERE HSHD_NUM='0010' ORDER BY """ + sorter #ORDER BY " + sorter + "WHERE HSHD_NUM='0010'"
         cursor = conn.cursor()    
         rows = cursor.execute(sql_statement).fetchall()
         cursor.close()
         return '<br><br>'.join(str(row) for row in rows)
+    if request.method == "GET":
+        return "directly trying to access data"
+    return "Fail"
+
+@app.route('/displayData/', methods = ["POST", "GET"])
+def displayData():
+    if request.method == "POST":
+        sorter =  request.form["sortBy"]
+        sql_statement = """SELECT TOP (1000) * FROM [household] WHERE HSHD_NUM='0010' ORDER BY """ + sorter #ORDER BY " + sorter + "WHERE HSHD_NUM='0010'"
+        cursor = conn.cursor()    
+        rows = cursor.execute(sql_statement).fetchall()
+        cursor.close()
+        return '<br><br>'.join(str(row) for row in rows)
+    if request.method == "GET":
+        return "directly trying to access data"
+    return "Fail"
 
 
-@app.route('/createDb')
-def createDb():
+def makeHouseHoldDb():
     cursor = conn.cursor()  
     cursor.execute("""DROP TABLE IF EXISTS household""")
     cursor.execute("""CREATE TABLE household (HSHD_NUM nvarchar(MAX), L nvarchar(MAX), AGE_RANGE nvarchar(MAX), MARITAL nvarchar(MAX), INCOME_RANGE nvarchar(MAX), HOMEOWNER nvarchar(MAX), HSHD_COMPOSITION nvarchar(MAX), HH_SIZE nvarchar(MAX), CHILDREN nvarchar(MAX))""")
 
-    with open('400_households.csv', 'r') as f:
+    with open('userHousehold.csv', 'r') as f:
         reader = csv.reader(f.readlines()[1:])  # exclude header line
         cursor.executemany("""INSERT INTO household VALUES (?,?,?,?,?,?,?,?,?)""",
                     (row for row in reader))
+    conn.commit()
+    cursor.close()
+
+def makeProductsDb():
+    cursor = conn.cursor()  
+    cursor.execute("""DROP TABLE IF EXISTS products""")
+    cursor.execute("""CREATE TABLE products (PRODUCT_NUM nvarchar(MAX), DEPARTMENT nvarchar(MAX), COMMODITY nvarchar(MAX), BRAND_TY nvarchar(MAX), NATURAL_ORGANIC_FLAG nvarchar(MAX))""")
+    with open('userProducts.csv', 'r') as f:
+        reader = csv.reader(f.readlines()[1:])  # exclude header line
+        cursor.executemany("""INSERT INTO products VALUES (?,?,?,?,?)""",
+                    (row for row in reader))
+    conn.commit()
+    cursor.close()
+
+def makeTransactionsDb():
+    cursor = conn.cursor()  
+    cursor.execute("""DROP TABLE IF EXISTS transactions""")
+    cursor.execute("""CREATE TABLE transactions (BASKET_NUM nvarchar(MAX), HSHD_NUM nvarchar(MAX), PURCHASE_ nvarchar(MAX),	PRODUCT_NUM nvarchar(MAX),  SPEND nvarchar(MAX), UNITS nvarchar(MAX), STORE_R nvarchar(MAX), WEEK_NUM nvarchar(MAX),YEAR nvarchar(MAX))""")
+
+    with open('userTransactions.csv', 'r') as f:
+        reader = csv.reader(f.readlines()[1:])  # exclude header line
+        cursor.executemany("""INSERT INTO transactions VALUES (?,?,?,?,?,?,?,?,?)""",
+                    (row for row in reader))
     cursor.commit()
     cursor.close()
-    return "success!"
-@app.route('/display')
-def display():
-    sql_statement = "SELECT TOP (900) * FROM [household]"
-
-    cursor = conn.cursor()    
-    rows = cursor.execute(sql_statement).fetchall()
-    cursor.close()
-    return '<br><br>'.join(str(row) for row in rows)
 
 @app.route('/loadData')
 def loadData():
-    return 'Load Data'
+    return """<form action = "/uploader" method = "POST" 
+         enctype = "multipart/form-data">
+         <p> Household file: <input type = "file" name = "householdFile" /></p>
+         <p> Products file: <input type = "file" name = "productsFile" /></p>
+         <p> Transactions file: <input type = "file" name = "transactionsFile" /></p>
+         <input type = "submit"/>
+      </form>""" 
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['householdFile']
+      g = request.files['productsFile']
+      h = request.files['transactionsFile']
+
+      g.filename = "userProducts.csv"
+      g.save(g.filename)
+      g.close()
+      makeProductsDb()
+      
+      f.filename = "userHousehold.csv"
+      f.save(f.filename)
+      f.close()
+      makeHouseHoldDb()
+
+      
+      h.filename = "userTransactions.csv"
+      h.save(h.filename)
+      h.close()
+      makeTransactionsDb()
+      
+      return 'file uploaded successfully'
 
 @app.route('/retailq1')
 def retailQ1():
